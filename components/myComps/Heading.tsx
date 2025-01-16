0f7705f380c0fe452370d508800e102877078958
Vivek <vivekpradhan736@gmail.com>
2025-01-16 23:45:31 +0530

    new commit

--- components/myComps/Heading.tsx
+++ components/myComps/Heading.tsx
@@ -1,11 +1,12 @@
 import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import React from "react";
+import { IconType } from "react-icons";
 
 interface HeadingProps {
   title: string;
   describtion: string;
-  icon: LucideIcon;
+  icon: IconType | LucideIcon;  // Accept both `react-icons` and `lucide-react` icons
   iconColor?: string;
   bgColor?: string;
 }
@@ -18,17 +19,15 @@ const Heading = ({
   bgColor,
 }: HeadingProps) => {
   return (
-    <>
-      <div className="p-4 lg:px-8 flex items-center gap-x-3">
-        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
-          <Icon className={cn("w-10 h-10", iconColor)} />
-        </div>
-        <div>
-          <h2 className="text-3xl font-bold text-black">{title}</h2>
-          <p className="text-sm text-gray-400">{describtion}</p>
-        </div>
+    <div className="p-4 lg:px-8 flex items-center gap-x-3">
+      <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+        <Icon className={cn("w-10 h-10", iconColor)} />
       </div>
-    </>
+      <div>
+        <h2 className="text-3xl font-bold text-black">{title}</h2>
+        <p className="text-sm text-gray-400">{describtion}</p>
+      </div>
+    </div>
   );
 };
 
