dc945734ff9e3e41b97465c3643dcdd0377aa9c4
Vivek <vivekpradhan736@gmail.com>
2025-01-16 23:24:10 +0530

    new commit

--- app/api/generate-audio/route.ts
+++ app/api/generate-audio/route.ts
@@ -9,23 +9,23 @@
 // 	apiKey:process.env.TEXT-TO-SPEECH
 // });
 
-// export async function POST(req: Request) {
-// 	const { text, id } = await req.json();
-// 	const storageRef = ref(storage,'ai-video-generator/'+id+'.mp3')
-// 	const request = {
-// 		input: { text: text },
-// 		voice: { languageCode: 'en-US', ssmlGender: 'FEMALE' },
-// 		audioConfig: { audioEncoding: 'MP3' },
-// 	};
-// 	const [response] = await client.synthesizeSpeech(request);
+export async function POST() {
+	// const { text, id } = await req.json();
+	// const storageRef = ref(storage,'ai-video-generator/'+id+'.mp3')
+	// const request = {
+	// 	input: { text: text },
+	// 	voice: { languageCode: 'en-US', ssmlGender: 'FEMALE' },
+	// 	audioConfig: { audioEncoding: 'MP3' },
+	// };
+	// const [response] = await client.synthesizeSpeech(request);
 	
-// 	const audioBuffer = Buffer.from(response.audioContent, 'binary');
-// 	await uploadBytes(storageRef, audioBuffer, { contentType: 'audio/mp3' });
-// 	const downloadUrl = await getDownloadURL(storageRef);
-// 	console.log(downloadUrl);
+	// const audioBuffer = Buffer.from(response.audioContent, 'binary');
+	// await uploadBytes(storageRef, audioBuffer, { contentType: 'audio/mp3' });
+	// const downloadUrl = await getDownloadURL(storageRef);
+	// console.log(downloadUrl);
 	
-// 	console.log('Audio content written to file:Output.mp3');
+	// console.log('Audio content written to file:Output.mp3');
 
-// 	return NextResponse.json({ Result: downloadUrl });
+	// return NextResponse.json({ Result: downloadUrl });
 	
-// }
+}
