26547441aa697ab47502161c4420037257d6f9f4
Vivek <vivekpradhan736@gmail.com>
2025-01-18 16:49:17 +0530

    new commit

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-    publicRoutes: ["/", "/api/webhook"]
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
